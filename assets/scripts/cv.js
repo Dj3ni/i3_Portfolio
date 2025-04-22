83dd5612588364de05e364ee933155039fac86da
Dj3n <fernandezgarcia.jenny@gmail.com>
2025-04-22 22:17:58 +0200

    bugfix cv pdf

--- assets/scripts/cv.js
+++ assets/scripts/cv.js
@@ -4,15 +4,15 @@ const PDF_DIV = document.querySelector(".pdf")
 // console.log(PDF_DIV);
 
 window.addEventListener("click",function(event) {
-    // console.log("toto");
+     //console.log("toto");
     if (!PDF_DIV.contains(event.target) && event.target !== CV_BUTTON) {
-        PDF_DIV.classList.remove("visible"); // Retire la classe "visible"
+        PDF_DIV.classList.remove("pdfVisible"); // Retire la classe "visible"
     }
 });
 
 CV_BUTTON.addEventListener("click", function(event){
     event.preventDefault();
-    PDF_DIV.classList.toggle("visible");
-    // console.log(PDF_DIV);
+    PDF_DIV.classList.toggle("pdfVisible");
+     //console.log(PDF_DIV);
 })
 
